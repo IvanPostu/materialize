6981730934c77a21f0aa954b4e97c4b9ceb60be5
ivan <ipostu20000127@gmail.com>
2024-09-04 20:53:34 +0300

    #

--- spec/tests1/scrollspy/scrollspySpec.js
+++ spec/tests1/scrollspy/scrollspySpec.js
@@ -1,23 +1,18 @@
 describe('Scrollspy Plugin', () => {
+  const DELAY_TIME = 600;
   const fixture = `
 <div class="container">
-    <div class="row" style="margin-top: 100vh;"></div>
+    <div id="heading" class="row" style="height: 100vh; margin: 0; padding: 0;"></div>
     <div class="row">
         <div class="col m7">
-            <div id="introduction" class="section scrollspy">
-                <p class="caption">
-                    introduction
-                </p>
+            <div id="introduction" class="section scrollspy" style="height: 100vh; margin: 0; padding: 0;  background-color: red;">
+                introduction
             </div>
-            <div id="initialization" class="section scrollspy">
-                <p class="caption">
-                    initialization
-                </p>
+            <div id="initialization" class="section scrollspy" style="height: 100vh; margin: 0; padding: 0;  background-color: green;">
+                initialization
             </div>
-            <div id="options" class="section scrollspy">
-                <p class="caption">
-                    options
-                </p>
+            <div id="options" class="section scrollspy" style="height: 100vh; margin: 0; padding: 0;  background-color: yellow;">
+                options
             </div>
         </div>
 
@@ -39,92 +34,77 @@ describe('Scrollspy Plugin', () => {
             </div>
         </div>
     </div>
+    <div id="footer" class="row" style="height: 100vh; margin: 0; padding: 0;"></div>
 </div>
 `;
-
-  function repeatString(str, count) {
-    let result = '';
-    for (let i = 0; i < count; i++) {
-      result += str
-
-    }
-    return result;
-  }
-
   let scrollspyInstances = [];
 
   beforeEach(() => {
     XloadHtml(fixture);
+    window.scrollTo(0, 0);
     scrollObservers = [];
     const elements = document.querySelectorAll('.scrollspy');
-    scrollspyInstances = M.ScrollSpy.init(elements, {
-    });
+    scrollspyInstances = M.ScrollSpy.init(elements, {});
   });
 
   afterEach(() => {
-    scrollspyInstances.forEach(value => value.destroy());
-    XunloadFixtures()
+    scrollspyInstances.forEach((value) => value.destroy());
+    XunloadFixtures();
   });
 
   describe('Scrollspy table of contents manipulations', () => {
-    let scrollObservers = [];
-
-    beforeEach(() => {
-      scrollObservers = [];
-    });
-
-    afterEach(() => {
-      scrollObservers.forEach(observer => observer.disconnect());
-    });
+    function getClassListByQuerySelector(querySelector) {
+      const element = document.querySelector(querySelector);
+      expect(element).not.toBeNull();
+      const classList = element.classList;
+      return Array.from(classList);
+    }
 
     it('Clicking on an item in the table of contents should scroll to the corresponding content section', (done) => {
-      document.querySelector('#introduction p').textContent = repeatString("test1 ", 900);
-      document.querySelector('#initialization p').textContent = repeatString("test2 ", 900);
-      document.querySelector('#options p').textContent = repeatString("test3 ", 900);
+      const introductionElement = document.querySelector('#introduction');
 
-      const viewedSection = [];
-      const observerCallback = (entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            viewedSection.push(entry.target.id)
-          }
-        });
-      };
-
-      const observer = new IntersectionObserver(observerCallback, {
-        root: null,
-        threshold: 0.5
-      });
-      scrollObservers.push(observer);
-
-      let elements = document.querySelectorAll('#introduction');
-      elements.forEach(element => {
-        observer.observe(element);
-      });
-
-      elements = document.querySelectorAll('#initialization');
-      elements.forEach(element => {
-        observer.observe(element);
-      });
-
-      elements = document.querySelectorAll('#options');
-      elements.forEach(element => {
-        observer.observe(element);
-      });
+      const viewportHeightPx = window.innerHeight;
+      const distanceToTopInPixels =
+        introductionElement.getBoundingClientRect().top - viewportHeightPx;
 
       document.querySelector('a[href="#introduction"]').click();
       setTimeout(() => {
-        expect(viewedSection).toEqual(['introduction']);
+        const scrollTop = window.scrollY;
+        console.log(viewportHeightPx, )
+        expect(scrollTop).toBe(viewportHeightPx + distanceToTopInPixels);
         document.querySelector('a[href="#initialization"]').click();
         setTimeout(() => {
-          expect(viewedSection).toEqual(['introduction', 'initialization']);
+          const scrollTop = window.scrollY;
+          expect(scrollTop).toBe(2 * viewportHeightPx + distanceToTopInPixels);
+
           document.querySelector('a[href="#options"]').click();
           setTimeout(() => {
-            expect(viewedSection).toEqual(['introduction', 'initialization', 'options']);
-            done()
-          }, 200)
-        }, 200)
-      }, 200)
+            const scrollTop = window.scrollY;
+            expect(scrollTop).toBe(3 * viewportHeightPx + distanceToTopInPixels);
+
+            done();
+          }, DELAY_TIME);
+        }, DELAY_TIME);
+      }, DELAY_TIME);
+    });
+
+    it('Clicking on an item in the table of contents should make item active', (done) => {
+      document.querySelector('a[href="#introduction"]').click();
+      setTimeout(() => {
+        expect(getClassListByQuerySelector('a[href="#introduction"]')).toEqual(['active']);
+
+        document.querySelector('a[href="#options"]').click();
+        setTimeout(() => {
+          expect(getClassListByQuerySelector('a[href="#options"]')).toEqual(['active']);
+
+          document.querySelector('a[href="#initialization"]').click();
+          setTimeout(() => {
+            expect(getClassListByQuerySelector('a[href="#initialization"]')).toEqual(['active']);
+
+            done();
+          }, DELAY_TIME);
+        }, DELAY_TIME);
+      }, DELAY_TIME);
     });
   });
 });
