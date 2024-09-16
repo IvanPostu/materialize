96c517a06f3d0c3c8807a30c8127b2534078dcca
ivan <ipostu20000127@gmail.com>
2024-09-16 22:05:19 +0300

    #

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import typescriptEslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -7,7 +9,13 @@ export default [
   },
   ...typescriptEslint.configs.recommended,
   {
+    plugins: {
+      prettier: prettierPlugin
+    },
     rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': 'warn',
       'prefer-const': 'warn',
       'prefer-rest-params': 'warn',
       'no-var': 'warn',
